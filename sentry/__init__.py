004da4e5735172546ee0fd91e8910c449cb62e39
Frankie Dintino <fdintino@theatlantic.com>
2011-01-27 15:13:31 -0500

    Manually setting sentry.VERSION in __init__.py rather than use pkg_resources, which throws irritating UserWarning "Module x was already imported from ..., but .../site-packages is being added to sys.path"

--- sentry/__init__.py
+++ sentry/__init__.py
@@ -2,9 +2,11 @@
 Sentry
 ~~~~~~
 """
-
-try:
-    VERSION = __import__('pkg_resources') \
-        .get_distribution('django-sentry').version
-except Exception, e:
-    VERSION = 'unknown'
+# pkg_resources spits out annoying "Module x was already imported ..." UserWarning
+# so we're setting the VERSION manually
+VERSION = '1.6.1'
+# try:
+#     VERSION = __import__('pkg_resources') \
+#         .get_distribution('django-sentry').version
+# except Exception, e:
+#     VERSION = 'unknown'
